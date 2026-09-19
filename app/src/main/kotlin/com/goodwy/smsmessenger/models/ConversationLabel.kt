9766a744766eea336573dcd234fa1053604906ef
freetux <thefreetux@gmail.com>
2026-09-19 23:47:22 +0330

    feat(homa): add labels and notes foundation

---
+++ app/src/main/kotlin/com/goodwy/smsmessenger/models/ConversationLabel.kt
@@ -0,0 +1,11 @@
+package com.goodwy.smsmessenger.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+
+@Entity(tableName = "conversation_labels", primaryKeys = ["thread_id", "label_id"], indices = [Index("label_id")])
+data class ConversationLabel(
+    @ColumnInfo(name = "thread_id") val threadId: Long,
+    @ColumnInfo(name = "label_id") val labelId: Long,
+)
